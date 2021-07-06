917d65325975e5cfc41cef6eaf0943080f7b5259
Szymon Gebler <gebler.szymon@gmail.com>
2021-07-06 17:43:48 +0200

    Configure `Strict Mode` rules

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 
 /** @type {import('eslint').Linter.Config} */
 const config = {
-  extends: ['./src/base/possible-errors.js', './src/base/best-practices.js'],
+  extends: [
+    './src/base/possible-errors.js',
+    './src/base/best-practices.js',
+    './src/base/strict-mode.js',
+  ],
 };
 
 module.exports = config;
