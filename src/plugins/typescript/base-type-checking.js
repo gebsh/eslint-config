1e81b94a22675e26a545808aa4f44058bc4b17ad
Szymon Gebler <gebler.szymon@gmail.com>
2021-07-08 10:40:09 +0200

    Configure `typescript/base` rules

---
+++ src/plugins/typescript/base-type-checking.js
@@ -0,0 +1,180 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      // Enforce camelCase by default.
+      {
+        selector: 'default',
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'forbid',
+      },
+      // Allow constants to be in UPPER_CASE.
+      {
+        selector: 'variable',
+        modifiers: ['const'],
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'forbid',
+      },
+      // Allow functions to be in PascalCase (e.g. React components).
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'forbid',
+      },
+      // Many tools such as ESLint or Webpack have options that may break
+      // camelCase convention. This allows specifying object properties in other
+      // formats.
+      {
+        selector: 'objectLiteralProperty',
+        format: null,
+      },
+      // Disallow beginning interfaces with 'I'.
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: false,
+        },
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'forbid',
+      },
+      // Require types and enum members to be in PascalCase.
+      {
+        selector: ['typeLike', 'enumMember'],
+        format: ['PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'forbid',
+      },
+      // Allow quoted properties (such as HTTP headers) to break default
+      // conventions.
+      {
+        selector: ['property', 'method', 'accessor'],
+        modifiers: ['requiresQuotes'],
+        format: null,
+      },
+    ],
+    '@typescript-eslint/no-base-to-string': ['error', { ignoredTypeNames: [] }],
+    '@typescript-eslint/no-confusing-void-expression': [
+      'error',
+      {
+        ignoreArrowShorthand: false,
+        ignoreVoidOperator: false,
+      },
+    ],
+    '@typescript-eslint/no-floating-promises': [
+      'error',
+      {
+        ignoreVoid: true,
+        ignoreIIFE: false,
+      },
+    ],
+    // OFF: This config disallows for..in loops. This is enforced by the base
+    // ESLint `no-restricted-syntax` rule.
+    '@typescript-eslint/no-for-in-array': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksConditionals: true,
+        checksVoidReturn: true,
+      },
+    ],
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': [
+      'warn',
+      {
+        allowComparingNullableBooleansToTrue: true,
+        allowComparingNullableBooleansToFalse: true,
+      },
+    ],
+    '@typescript-eslint/no-unnecessary-condition': [
+      'warn',
+      {
+        allowConstantLoopConditions: true,
+        allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: false,
+      },
+    ],
+    '@typescript-eslint/no-unnecessary-qualifier': 'warn',
+    '@typescript-eslint/no-unnecessary-type-arguments': 'warn',
+    '@typescript-eslint/no-unnecessary-type-assertion': [
+      'warn',
+      { typesToIgnore: [] },
+    ],
+    '@typescritp-eslint/no-unsafe-argument': 'error',
+    '@typescript-eslint/no-unsafe-assignment': 'error',
+    '@typescript-eslint/no-unsafe-call': 'error',
+    '@typescript-eslint/no-unsafe-member-access': 'error',
+    '@typescript-eslint/no-unsafe-return': 'error',
+    '@typescript-eslint/non-nullable-type-assertion-style': 'warn',
+    '@typescript-eslint/prefer-includes': 'warn',
+    '@typescript-eslint/prefer-nullish-coalescing': [
+      'warn',
+      {
+        ignoreConditionalTests: true,
+        ignoreMixedLogicalExpressions: true,
+      },
+    ],
+    '@typescript-eslint/prefer-readonly': [
+      'warn',
+      { onlyInlineLambdas: false },
+    ],
+    '@typescript-eslint/prefer-readonly-parameter-types': [
+      'warn',
+      {
+        checkParameterProperties: true,
+        ignoreInferredTypes: false,
+      },
+    ],
+    '@typescript-eslint/prefer-reduce-type-parameter': 'warn',
+    '@typescript-eslint/prefer-regexp-exec': 'warn',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'warn',
+    '@typescript-eslint/promise-function-async': [
+      'error',
+      {
+        allowAny: true,
+        allowedPromiseNames: [],
+        checkArrowFunctions: true,
+        checkFunctionDeclarations: true,
+        checkFunctionExpressions: true,
+        checkMethodDeclarations: true,
+      },
+    ],
+    '@typescript-eslint/require-array-sort-compare': [
+      'error',
+      { ignoreStringArrays: false },
+    ],
+    '@typescript-eslint/restrict-plus-operands': [
+      'error',
+      { checkCompoundAssignments: true },
+    ],
+    '@typescript-eslint/restrict-template-expressions': [
+      'warn',
+      {
+        allowNumber: true,
+        allowBoolean: true,
+        allowAny: false,
+        allowNullish: true,
+      },
+    ],
+    '@typescript-eslint/strict-boolean-expressions': [
+      'warn',
+      {
+        allowString: false,
+        allowNumber: false,
+        allowNullableObject: true,
+        allowNullableBoolean: false,
+        allowNullableString: false,
+        allowNullableNumber: false,
+        allowAny: false,
+        allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: false,
+      },
+    ],
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+    '@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
+  },
+};
---
+++ src/plugins/typescript/base.js
@@ -0,0 +1,303 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    '@typescript-eslint/adjacent-overload-signatures': 'warn',
+    '@typescript-eslint/array-type': [
+      'warn',
+      { default: 'array-simple', readonly: 'array-simple' },
+    ],
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+        'ts-expect-error': 'allow-with-description',
+        'ts-ignore': 'allow-with-description',
+        'ts-nocheck': true,
+        'ts-check': false,
+        minimumDescriptionLength: 3,
+      },
+    ],
+    '@typescript-eslint/ban-tslint-comment': 'error',
+    '@typescript-eslint/ban-types': [
+      'warn',
+      {
+        types: {
+          Boolean: {
+            message: "Use 'boolean' instead.",
+            fixWith: 'boolean',
+          },
+          Number: {
+            message: "Use 'number' instead.",
+            fixWith: 'number',
+          },
+          BigInt: {
+            message: "Use 'bigint' instead.",
+            fixWith: 'bigint',
+          },
+          String: {
+            message: "Use 'string' instead.",
+            fixWith: 'string',
+          },
+          Symbol: {
+            message: "Use 'symbol' instead.",
+            fixWith: 'symbol',
+          },
+          Function: {
+            message:
+              'This type does not provide any type safety since it accepts ' +
+              'any function (and also some other types which will throw at ' +
+              'runtime). Specify an explicit function type instead.',
+          },
+          Object: {
+            message:
+              "Use 'object' or 'Record<string, unknown>' instead, or create " +
+              'an interface or type alias for your object.',
+            fixWith: 'object',
+          },
+          '{}': {
+            message:
+              "Use 'object' or 'Record<string, unknown>' instead, or create " +
+              'an interface or type alias for your object.',
+            fixWith: 'object',
+          },
+        },
+        extendDefaults: false,
+      },
+    ],
+    '@typescript-eslint/class-literal-property-style': ['warn', 'fields'],
+    '@typescript-eslint/consistent-indexed-object-style': ['warn', 'record'],
+    '@typescript-eslint/consistent-type-assertions': [
+      'warn',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'never',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: true,
+      },
+    ],
+    // OFF: Enabled only in TypeScript files.
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // OFF: Enabled only in TypeScript files.
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    // OFF: Enabled only in TypeScript files.
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'warn',
+      {
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        overrides: {},
+        multilineDetection: 'brackets',
+      },
+    ],
+    '@typescript-eslint/member-ordering': [
+      'warn',
+      {
+        default: {
+          order: 'as-written',
+          memberTypes: [
+            'static-field',
+            'abstract-field',
+            'decorated-field',
+            'instance-field',
+            'signature',
+            'constructor',
+            'static-method',
+            'abstract-method',
+            'decorated-method',
+            'instance-method',
+          ],
+        },
+        interfaces: {
+          order: 'as-written',
+          memberTypes: ['constructor', 'field', 'signature', 'method'],
+        },
+        typeLiterals: {
+          order: 'as-written',
+          memberTypes: ['constructor', 'field', 'signature', 'method'],
+        },
+      },
+    ],
+    '@typescript-eslint/method-signature-style': ['warn', 'property'],
+    '@typescript-eslint/no-confusing-non-null-assertion': 'error',
+    '@typescript-eslint/no-dynamic-delete': 'error',
+    '@typescript-eslint/no-empty-interface': [
+      'warn',
+      { allowSingleExtends: true },
+    ],
+    '@typescript-eslint/no-explicit-any': [
+      'warn',
+      {
+        // Fixing to `unknown` is not a desirable effect in some cases and can
+        // be more annoying than requiring to manually change the type or to
+        // disable the rule altogether.
+        fixToUnknown: false,
+        ignoreRestArgs: false,
+      },
+    ],
+    '@typescript-eslint/no-extra-non-null-assertion': 'warn',
+    '@typescript-eslint/no-extraneous-class': [
+      'warn',
+      {
+        allowConstructorOnly: false,
+        allowEmpty: false,
+        allowStaticOnly: false,
+        allowWithDecorator: false,
+      },
+    ],
+    '@typescript-eslint/no-implicit-any-catch': [
+      'error',
+      { allowExplicitAny: false },
+    ],
+    '@typescript-eslint/no-inferrable-types': [
+      'warn',
+      {
+        ignoreParameters: true,
+        ignoreProperties: true,
+      },
+    ],
+    '@typescript-eslint/no-invalid-void-type': [
+      'error',
+      {
+        allowInGenericTypeArguments: true,
+        allowAsThisParameter: false,
+      },
+    ],
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': [
+      'error',
+      {
+        allowDeclarations: false,
+        allowDefinitionFiles: true,
+      },
+    ],
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+    // OFF: Non-null assertions are commonly used with Set/Map to get an element
+    // after checking whether the collection contains it with the `has` method.
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-parameter-properties': 'error',
+    // OFF: Enabled only in TypeScript files.
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-this-alias': [
+      'error',
+      {
+        allowDestructuring: true,
+        allowedNames: ['self'],
+      },
+    ],
+    '@typescript-eslint/no-type-alias': [
+      'warn',
+      {
+        allowAliases: 'always',
+        allowCallbacks: 'always',
+        allowConditionalTypes: 'always',
+        allowConstructors: 'always',
+        allowLiterals: 'never',
+        allowMappedTypes: 'always',
+        allowTupleTypes: 'always',
+      },
+    ],
+    '@typescript-eslint/no-unnecessary-type-constraint': 'warn',
+    // OFF: This config disallows variables to be declared with the `var
+    // keyword. This is enforced by the base ESLint `no-var` rule.
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/prefer-as-const': 'warn',
+    '@typescript-eslint/prefer-enum-initializers': 'warn',
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/prefer-function-type': 'warn',
+    '@typescript-eslint/prefer-literal-enum-member': [
+      'error',
+      { allowBitwiseExpressions: true },
+    ],
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    '@typescript-eslint/sort-type-union-intersection-members': [
+      'warn',
+      {
+        checkIntersections: true,
+        checkUnions: true,
+        groupOrder: [
+          'named',
+          'keyword',
+          'literal',
+          'function',
+          'import',
+          'operator',
+          'conditional',
+          'tuple',
+          'object',
+          'intersection',
+          'union',
+          'nullish',
+        ],
+      },
+    ],
+    '@typescript-eslint/triple-slash-reference': [
+      'error',
+      { path: 'never', types: 'never', lib: 'never' },
+    ],
+    '@typescript-eslint/type-annotation-spacing': [
+      'warn',
+      { before: false, after: true, overrides: {} },
+    ],
+    // OFF: As noted in the rule description, this can be cumbersome to maintain
+    // and TypeScript's compiler flags should be used instead.
+    '@typescript-eslint/typedef': 'off',
+    '@typescript-eslint/unified-signatures': 'warn',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': [
+          'error',
+          {
+            allowExpressions: false,
+            allowTypedFunctionExpressions: true,
+            allowHigherOrderFunctions: true,
+            allowDirectConstAssertionInArrowFunctions: true,
+            allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+          },
+        ],
+        '@typescript-eslint/explicit-member-accessibility': [
+          'warn',
+          {
+            accessibility: 'explicit',
+            ignoredMethodNames: [],
+            overrides: {
+              accessors: 'explicit',
+              constructors: 'no-public',
+              methods: 'explicit',
+              properties: 'explicit',
+              parameterProperties: 'explicit',
+            },
+          },
+        ],
+        '@typescript-eslint/explicit-module-boundary-types': [
+          'error',
+          {
+            allowArgumentsExplicitlyTypedAsAny: false,
+            allowDirectConstAssertionInArrowFunctions: true,
+            allowedNames: [],
+            allowHigherOrderFunctions: true,
+            allowTypedFunctionExpressions: true,
+          },
+        ],
+        '@typescript-eslint/no-require-imports': 'error',
+      },
+    },
+  ],
+};
