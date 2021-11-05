510c9ae3fd73a31d1e062d2618706bf031a4be0a
Szymon Gebler <gebler.szymon@gmail.com>
2021-11-05 12:29:53 +0100

    Update configuration of the `prettier` rules

--- packages/eslint-config/src/plugins/prettier/base.js
+++ packages/eslint-config/src/plugins/prettier/base.js
@@ -1,32 +1,39 @@
 'use strict';
 
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   rules: {
-    // Possible Errors
-    'no-extra-parens': 'off',
-    'no-extra-semi': 'off',
+    // Possible Problems
     'no-unexpected-multiline': 'off',
 
-    // Best Practices
-    'dot-location': 'off',
+    // Suggestions
+    'arrow-body-style': 'off',
+    'no-confusing-arrow': 'off',
+    'no-extra-semi': 'off',
     'no-floating-decimal': 'off',
-    'no-multi-spaces': 'off',
-    'wrap-iife': 'off',
+    'no-mixed-operators': 'off',
+    'one-var-declaration-per-line': 'off',
+    'prefer-arrow-callback': 'off',
+    'quote-props': 'off',
 
-    // Stylistic Issues
+    // Layout & Formatting
     'array-bracket-newline': 'off',
     'array-bracket-spacing': 'off',
     'array-element-newline': 'off',
+    'arrow-parens': 'off',
+    'arrow-spacing': 'off',
     'block-spacing': 'off',
     'brace-style': 'off',
     'comma-dangle': 'off',
     'comma-spacing': 'off',
     'comma-style': 'off',
     'computed-property-spacing': 'off',
+    'dot-location': 'off',
     'eol-last': 'off',
     'func-call-spacing': 'off',
     'function-call-argument-newline': 'off',
     'function-paren-newline': 'off',
+    'generator-star-spacing': 'off',
     'implicit-arrow-linebreak': 'off',
     indent: 'off',
     'jsx-quotes': 'off',
@@ -36,18 +43,18 @@ module.exports = {
     'multiline-ternary': 'off',
     'new-parens': 'off',
     'newline-per-chained-call': 'off',
-    'no-mixed-operators': 'off',
+    'no-extra-parens': 'off',
     'no-mixed-spaces-and-tabs': 'off',
+    'no-multi-spaces': 'off',
     'no-multiple-empty-lines': 'off',
     'no-trailing-spaces': 'off',
     'no-whitespace-before-property': 'off',
     'object-curly-newline': 'off',
     'object-curly-spacing': 'off',
     'object-property-newline': 'off',
-    'one-var-declaration-per-line': 'off',
     'operator-linebreak': 'off',
     'padded-blocks': 'off',
-    'quote-props': 'off',
+    'rest-spread-spacing': 'off',
     semi: 'off',
     'semi-spacing': 'off',
     'semi-style': 'off',
@@ -57,19 +64,13 @@ module.exports = {
     'space-infix-ops': 'off',
     'space-unary-ops': 'off',
     'switch-colon-spacing': 'off',
-    'template-tag-spacing': 'off',
-    // https://github.com/prettier/prettier/issues/6461
-    // 'unicode-bom': 'off'
-
-    // ECMAScript 6
-    'arrow-body-style': 'off',
-    'arrow-parens': 'off',
-    'arrow-spacing': 'off',
-    'generator-star-spacing': 'off',
-    'no-confusing-arrow': 'off',
-    'prefer-arrow-callback': 'off',
-    'rest-spread-spacing': 'off',
     'template-curly-spacing': 'off',
+    'template-tag-spacing': 'off',
+    // OFF: This is disabled in the official `eslint-config-prettier`
+    // conguration but in reality it shouldn't be disabled. For more information
+    // see: https://github.com/prettier/prettier/issues/6461.
+    // 'unicode-bom': 'off'
+    'wrap-iife': 'off',
     'yield-star-spacing': 'off',
   },
 };
